5c48560c251c176b8fc7a75fc88be5154fe89590
LAPTOP-7R47QMVR\chenbaigao <505042464@qq.com>
2021-09-28 10:52:01 +0100

    1. git test1

--- src/main/java/com/bssystem/BsSystemApplication.java
+++ src/main/java/com/bssystem/BsSystemApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BsSystemApplication {
 
     public static void main(String[] args) {
+        System.out.println("test");
         SpringApplication.run(BsSystemApplication.class, args);
     }
 
